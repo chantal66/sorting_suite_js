db5243163260a718c5c6aafd67393f6274a616fe
Chantal Justamond <cjustamm66@me.com>
2017-09-30 14:24:25 -0600

    initial commit

---
+++ test/bubbleSort_test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const bubbleSort = require('../bubbleSort');
+
+describe('bubble sort func'), function () {
+    it('it can sort a given array of numbers')
+}
+
+
+
