78b98ebddee8fb1549cbad14102cbdae46a8c1df
Chantal Justamond <cjustamm66@me.com>
2017-09-30 15:38:47 -0600

    test passing :hamster:

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,14 +1,9 @@
-let pry = require('pryjs');
-let items = [3,5,6,0,4,1];
 
 function bubbleSort(items) {
     let length = items.length;
     for (let i = (length - 1); i >= 0; i--) {
-        //Number of passes
         for (let j = (length - i); j > 0; j--) {
-            //Compare the adjacent positions
             if (items[j] < items[j - 1]) {
-                //Swap the numbers
                 let tmp = items[j];
                 items[j] = items[j - 1];
                 items[j - 1] = tmp;
@@ -18,7 +13,4 @@ function bubbleSort(items) {
     return items;
 }
 
-console.log(bubbleSort(items));
-// bubbleSort(items);
-// eval(pry.it);
-// module.exports = bubbleSort(items);
+module.exports = bubbleSort;
--- test/bubbleSort_test.js
+++ test/bubbleSort_test.js
@@ -1,8 +1,10 @@
+let pry = require('pryjs');
 const assert = require('chai').assert;
 const bubbleSort = require('../bubbleSort');
 
 describe('bubble sort func', function () {
     it('it can sort a given array of numbers', function () {
+        // eval(pry.it);
         assert.deepEqual(bubbleSort([3,5,6,0,4,1]), [0,1,3,4,5,6]);
     })
 });
