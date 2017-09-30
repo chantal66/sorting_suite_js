8468a46367f4a38cfda441fe82563af1daeadd02
Chantal Justamond <cjustamm66@me.com>
2017-09-30 14:31:12 -0600

    test added

--- test/bubbleSort_test.js
+++ test/bubbleSort_test.js
@@ -1,9 +1,11 @@
 const assert = require('chai').assert;
 const bubbleSort = require('../bubbleSort');
 
-describe('bubble sort func'), function () {
-    it('it can sort a given array of numbers')
-}
+describe('bubble sort func', function () {
+    it('it can sort a given array of numbers', function () {
+        assert.deepEqual(bubbleSort([3,5,6,0,4,1]), [0,1,3,4,5,6]);
+    })
+});
 
 
 
