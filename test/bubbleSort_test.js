0b9f1d6d577afedbc3ba61ec6a900402f38a832a
Chantal Justamond <cjustamm66@me.com>
2017-09-30 18:48:02 -0600

    test for sorting letters added

--- test/bubbleSort_test.js
+++ test/bubbleSort_test.js
@@ -4,8 +4,11 @@ const bubbleSort = require('../bubbleSort');
 
 describe('bubble sort func', function () {
     it('it can sort a given array of numbers', function () {
-        // eval(pry.it);
         assert.deepEqual(bubbleSort([3,5,6,0,4,1]), [0,1,3,4,5,6]);
+    });
+
+    it('can sort a given array of letters', function () {
+        assert.deepEqual(bubbleSort(["d", "b", "a", "c"]), ['a', 'b', 'c', 'd'])
     })
 });
 
